e3965785ae6af3bd54c3d314551fa56ae7af6c30
stomple <stomple@TeamCity>
2026-05-21 16:54:41 +0000

    Synchronization with own VCS root is enabled (TeamCity change in 'stomple' project)

---
+++ .teamcity/settings.kts
@@ -0,0 +1,75 @@
+import jetbrains.buildServer.configs.kotlin.*
+import jetbrains.buildServer.configs.kotlin.buildSteps.maven
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2026.1"
+
+project {
+
+    buildType(Stomple)
+    buildType(MavenBuild)
+}
+
+object MavenBuild : BuildType({
+    name = "Maven Build"
+
+    params {
+        param("branch", "%teamcity.build.branch%")
+        param("env.NEXUS_PASSWORD", "KbUfy5zW")
+    }
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+
+    steps {
+        maven {
+            id = "Maven2"
+
+            conditions {
+                doesNotEqual("branch", "master")
+            }
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+        maven {
+            name = "Deploy to Nexus (master only)"
+            id = "Deploy_to_Nexus_master_only"
+
+            conditions {
+                equals("branch", "master")
+            }
+            goals = "clean deploy"
+            userSettingsSelection = "nexus-settings"
+        }
+    }
+})
+
+object Stomple : BuildType({
+    name = "stomple"
+
+    vcs {
+        root(DslContext.settingsRoot)
+    }
+})
