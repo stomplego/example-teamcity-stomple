29e17ddffe33ebe2af36d472811b42e3e98c575e
stomplego <stomplego@gmail.com>
2026-05-21 21:13:51 +0400

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -2,14 +2,13 @@ package plaindoll;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
 public class WelcomerTest {
 	
 	private Welcomer welcomer = new Welcomer();
-	// Если хочешь больше веселья и информации про ДевОпс - приходи в мои каналы NotOps (telegram, YT, Boosty, Patreon)
-	// https://t.me/notopsofficial
 
 	@Test
 	public void welcomerSaysWelcome() {
